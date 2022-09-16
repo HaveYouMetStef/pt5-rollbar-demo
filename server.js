aa04b8ad99637a69abb0bcf64aa1976752ad325e
Stef Castillo <estefan.castillo@yahoo.com>
2022-09-15 23:05:38 -0500

    deployment 2 commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'ca1730ecb9d14f419af841c29d09a20c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    rollbar.info("HTML served successfully")
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    rollbar.info("Someone got the list of students to load")
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+            rollbar.log("student added successfully", {author: "Stef", type: "manual entry"})
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+            rollbar.error("No name provided")
+           res.status(400).send('You must enter a name.')
+       } else {
+            rollbar.error("student already exists")
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    rollbar.info("name removed")
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler());
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
